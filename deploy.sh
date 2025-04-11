de3bf49248f297203ee3c067e938e80a1db352aa
xx1003 <wntnwls2002@gmail.com>
2025-04-11 09:59:08 +0900

    testing | 2025-04-11 00:59:08 (KST)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
+# 커밋 메시지 입력 받기
+read -p "커밋 메시지를 입력하세요: " user_message
+
+ # 현재 시간 (KST 기준)
+timestamp=$(TZ=Asia/Seoul date +"%Y-%m-%d %H:%M:%S (KST)")
+
+ # 메시지 포맷: 메시지 | 날짜 (KST)
+commit_message="$user_message | $timestamp"
+
+ # Git 커밋 및 푸시
 git add .
-git commit -m "updated"
+git commit -m "$commit_message"
 git push
